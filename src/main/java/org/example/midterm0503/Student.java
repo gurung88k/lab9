6af2d5bcba289e005915574972e8a1d9e09e4c4c
gurung88k <gurungk4305@gmail.com>
2024-06-20 16:07:22 -0400

    2a

--- src/main/java/org/example/midterm0503/Student.java
+++ src/main/java/org/example/midterm0503/Student.java
@@ -1,5 +1,105 @@
 package org.example.midterm0503;
 
 public class Student {
+    private int studentNum;
+    private String firstName;
+    private String lastName;
+    private String telephone;
+    private String address;
+    private String province;
+    private int avgGrade;
+    private String major;
 
+    // Constructor
+    public Student(int studentNum, String firstName, String lastName, String telephone,
+                   String address, String province, int avgGrade, String major) {
+        this.studentNum = studentNum;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
+        this.address = address;
+        this.province = province;
+        this.avgGrade = avgGrade;
+        this.major = major;
     }
+
+    // Getters and setters (you may not need all depending on your use case)
+    public int getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(int studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public int getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(int avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    // Override toString() method for debugging purposes (optional)
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentNum=" + studentNum +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", address='" + address + '\'' +
+                ", province='" + province + '\'' +
+                ", avgGrade=" + avgGrade +
+                ", major='" + major + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/midterm0503/StudentViewController.java
+++ src/main/java/org/example/midterm0503/StudentViewController.java
@@ -1,11 +1,17 @@
-package com.example.s24comp1011gctest1;
-
+package org.example.midterm0503;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-public class StudentViewController  {
-
+public class StudentViewController {
     @FXML
     private TableView<Student> tableView;
 
@@ -45,12 +51,53 @@ public class StudentViewController  {
     @FXML
     private ComboBox<String> areaCodeComboBox;
 
+    private ObservableList<Student> students = FXCollections.observableArrayList();
+
     @FXML
-    private void applyFilter()  {
+    private void applyFilter() {
+        // Apply filter logic here
+    }
+
+    private void getAllStudents() {
+        String url = "jdbc:mysql://database.c10eksou8rzh.us-east-1.rds.amazonaws.com:3306/yourDatabaseName";
+        String user = "admin";
+        String password = "Gurung123$";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM students")) {
+            while (resultSet.next()) {
+                Student student = new Student();
+                student.setStudentNum(resultSet.getInt("studentNum"));
+                student.setFirstName(resultSet.getString("firstName"));
+                student.setLastName(resultSet.getString("lastName"));
+                student.setHomeAddress(resultSet.getString("homeAddress"));
+                student.setProvince(resultSet.getString("province"));
+                student.setTelephone(resultSet.getString("telephone"));
+                student.setAvgGrade(resultSet.getInt("avgGrade"));
+                student.setMajor(resultSet.getString("major"));
+                students.add(student);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void initialize() {
         areaCodeComboBox.getItems().add("All");
+        // Populate the TableView with all students
+        getAllStudents();
+        tableView.setItems(students);
+
+        // Set cell value factories for columns
+        studentNumCol.setCellValueFactory(cellData -> cellData.getValue().studentNumProperty().asObject());
+        firstNameCol.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameCol.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        telephoneCol.setCellValueFactory(cellData -> cellData.getValue().telephoneProperty());
+        addressCol.setCellValueFactory(cellData -> cellData.getValue().homeAddressProperty());
+        provinceCol.setCellValueFactory(cellData -> cellData.getValue().provinceProperty());
+        avgGradeCol.setCellValueFactory(cellData -> cellData.getValue().avgGradeProperty().asObject());
+        majorCol.setCellValueFactory(cellData -> cellData.getValue().majorProperty());
     }
 }
